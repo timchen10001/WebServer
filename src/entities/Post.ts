c21ffeb333c46c6a65af110ef3f864271e99b9d2
陳宜庭 <studenttim@chenyitingdeMacBook-Pro.local>
2021-02-21 14:10:25 +0800

    增加 FieldResolver creator

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -38,7 +38,7 @@ export class Post extends BaseEntity {
   @Column()
   creatorId: number;
 
-  @Field(() => User, { nullable: true })
+  @Field(() => User)
   @ManyToOne(() => User, (user) => user.posts)
   creator: User;
 
